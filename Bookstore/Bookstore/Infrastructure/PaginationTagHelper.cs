4306ed705e9eb44da2e1b689816a3b8d769eacae
cjtracy <connertracy@comcast.net>
2022-02-16 20:27:46 -0700

    Added Bootstrap

--- Bookstore/Bookstore/Infrastructure/PaginationTagHelper.cs
+++ Bookstore/Bookstore/Infrastructure/PaginationTagHelper.cs
@@ -27,6 +27,10 @@ namespace Bookstore.Infrastructure
 
         public BookInfo PageModel { get; set; }
         public string PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
         public override void Process(TagHelperContext thc, TagHelperOutput tho)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
@@ -38,6 +42,12 @@ namespace Bookstore.Infrastructure
                 TagBuilder tb = new TagBuilder("a");
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                if (PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageModel.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                }
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
