5b37b25ebb62e424e19cd950a18e0962e861a06d
cjtracy <connertracy@comcast.net>
2022-03-01 20:38:06 -0700

    Added Purchase form

---
+++ Bookstore/Bookstore/Migrations/20220302031953_noAnonymous.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bookstore.Migrations
+{
+    public partial class noAnonymous : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Anonymous",
+                table: "Purchases");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Anonymous",
+                table: "Purchases",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- Bookstore/Bookstore/Models/BookstoreContext.cs
+++ Bookstore/Bookstore/Models/BookstoreContext.cs
@@ -21,5 +21,7 @@ namespace Bookstore.Models
         }
 
         public DbSet<Book> Books { get; set; }
+
+        public DbSet<Purchase> Purchases { get; set; }
     }
 }
